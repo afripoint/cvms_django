8d6949bbca53093095cc4534ce034ece493f10ba
Dennis Akagha <d.akagha20@gmail.com>
2024-08-23 04:08:22 +0100

    implement 2factor auth feature

--- logs/urls.py
+++ logs/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import AllLogAPIView
+from .views import AllAuditLogAPIView, AllLogAPIView
 
 
 urlpatterns = [
     path("all-logs/", AllLogAPIView.as_view(), name="all logs"),
+    path("all-audit-logs/", AllAuditLogAPIView.as_view(), name="all-audit-logs"),
 ]
--- logs/views.py
+++ logs/views.py
@@ -3,10 +3,10 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
 from drf_yasg.utils import swagger_auto_schema
-from logs.models import Log
+from logs.models import AuditLog, Log
 from logs.serializers import LogSerializer
 
-
+# All logs
 class AllLogAPIView(APIView):
     @swagger_auto_schema(
         operation_summary="This is endpoint gets all logs generated",
@@ -17,5 +17,21 @@ class AllLogAPIView(APIView):
         serializer = LogSerializer(logs, many=True)
         response = {
             "All Logs": serializer.data,
+        }
+        return Response(data=response, status=status.HTTP_200_OK)
+
+    # All audit_Logs
+
+
+class AllAuditLogAPIView(APIView):
+    @swagger_auto_schema(
+        operation_summary="This is endpoint gets all audit logs generated",
+        operation_description="This endpoint gets all audit logs being generated",
+    )
+    def get(self, request, *args, **kwargs):
+        audit_logs = AuditLog.objects.all()
+        serializer = LogSerializer(audit_logs, many=True)
+        response = {
+            "All_Audit_Logs": serializer.data,
         }
         return Response(data=response, status=status.HTTP_200_OK)
