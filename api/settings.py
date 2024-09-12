e0629591d6c181512240e37ddfa96c9c23b0e07e
Dennis Akagha <d.akagha20@gmail.com>
2024-09-12 11:35:55 +0100

    add busols files configuration2

--- api/settings.py
+++ api/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-f*$dp9#im)nkbux#8lml8j#wn^1jvtxu6tb6%&k47f+inid09-"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
