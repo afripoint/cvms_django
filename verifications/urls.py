0b2a29723a49b406cac3a6dd5c87e6cf53603fe5
Dennis Akagha <d.akagha20@gmail.com>
2024-09-19 04:22:11 +0100

    complete the implementation of certificate verification

--- verifications/urls.py
+++ verifications/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
+from verifications.views import VerifyCertificateWithQRCodeAPIView
+
 urlpatterns = [
-    
+    # Define the route for validating the certificate by VIN number
+    path('verify-certificate/', VerifyCertificateWithQRCodeAPIView.as_view(), name='validate-certificate'),
 ]
