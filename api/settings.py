470faa6f652482af90b7de384a51cb9894b1b00c
firmprojects <kcvontop@gmail.com>
2024-10-10 11:37:49 +0100

    cmmt

--- api/settings.py
+++ api/settings.py
@@ -112,7 +112,7 @@ DATABASES = {
         "USER": config("POSTGRES_USER"),
         "PASSWORD": config("POSTGRES_PASSWORD"),
         "HOST": config("POSTGRES_HOST"),
-        # "PORT": config("POSTGRES_PORT", 5432),
+        "PORT": 5432,
         # "OPTIONS": {
         #     "sslmode": "require",
         # },
