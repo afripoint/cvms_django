de04b03f01175a3afbaa81c1ef4c4fd80b263013
Dennis Akagha <d.akagha20@gmail.com>
2024-10-25 13:21:04 +0100

    resolve server issues

--- api/settings.py
+++ api/settings.py
@@ -100,26 +100,26 @@ WSGI_APPLICATION = "api.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": config("POSTGRES_DB"),
-        "USER": config("POSTGRES_USER"),
-        "PASSWORD": config("POSTGRES_PASSWORD"),
-        "HOST": config("POSTGRES_HOST"),
-        "PORT": 5432,
-        # "OPTIONS": {
-        #     "sslmode": "require",
-        # },
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": config("POSTGRES_DB"),
+#         "USER": config("POSTGRES_USER"),
+#         "PASSWORD": config("POSTGRES_PASSWORD"),
+#         "HOST": config("POSTGRES_HOST"),
+#         "PORT": 5432,
+#         # "OPTIONS": {
+#         #     "sslmode": "require",
+#         # },
+#     }
+# }
 
 # DATABASES = {"default": config("DATABASE_URL")}
 
